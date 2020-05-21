f2a24104cc6af789b91afcee0dc70afa5f04a367
yutta13 <dodik13@gmail.com>
2020-05-21 20:56:31 +0300

    2_2

--- lessn1/src/Home2n2_2.java
+++ lessn1/src/Home2n2_2.java
@@ -1,15 +1,17 @@
 /* Вводится число. Если оно больше нуля, но меньше 4-ех, тогда вывести все нечетные числа.
 Если оно больше пяти, но меньше 11 – вывести четное. В иных ситуациях вывести сообщение об ошибке
  */
-public class Home2n2 {
+public class Home2n2_2 {
     public static void main(String[] args) {
-        int n = 12;
+        int n = 8;
+   //     char i = ;
 
         if ((n>0)&(n<=4))
-
             System.out.println("2,4");
-        if ((n>=5)&(n<11))
+
+        else if ((n>=5)&(n<11))
             System.out.println("5,7,9,11");
+
         else {
             System.out.println("err");
         }

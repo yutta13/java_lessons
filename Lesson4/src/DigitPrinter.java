a7483866ea1053378ad7bf08bec98db6923ae4c2
yutta13 <dodik13@gmail.com>
2020-05-26 11:37:37 +0300

    HW4
    functions, classes, recursive fn

--- Lesson4/src/DigitPrinter.java
+++ Lesson4/src/DigitPrinter.java
@@ -5,4 +5,23 @@
  */
 
 public class DigitPrinter {
+    static int n = 18808975;
+
+
+    private static void printDigits(int n) {
+        int a=0;
+        if (n >= 1){
+            a = n%10;
+            n/=10;
+            System.out.print(a);
+        printDigits(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        printDigits(n);
+    }
+
 }
+
+

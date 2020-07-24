995f7e561a0951a1175d7b6acf8c1308d089e1bf
yutta13 <dodik13@gmail.com>
2020-07-24 17:15:51 +0300

    ff

--- L18File/src/FileClass.java
+++ L18File/src/FileClass.java
@@ -4,36 +4,35 @@ import java.io.IOException;
 
 public class FileClass {
 
-
     public static void main(String[] args) throws IOException {
 
-
         File newTxtDir = new File("D:\\forTxt"); //временно
-
         File path = new File("D:\\rootFolder");
 
-        File[] arrayOfDir = path.listFiles(); //get list of dir
-
-        for (File file : arrayOfDir) //for each file check if it is a dir
-
-         { if (file.isDirectory()){ //if it is a dir - get files .txt
-            
-                File[] arrayOftxt =  file.listFiles(new TxtFilter());  
-
-                for (File txtFile : arrayOftxt){ //for each file.txt replace it to new dir
-                    System.out.println(txtFile);
-                    txtFile.renameTo(newTxtDir);
-                }
-            }
-         }
 
         File[] arrayt = path.listFiles(new TxtFilter());
-        for (File txtFile : arrayt){ //for each file.txt replace it to new dir
-            System.out.println(txtFile);
-            txtFile.renameTo(newTxtDir);
+        if (arrayt.length>0){
+         newTxtDir.mkdir();
+        }
+        for (File txtFile : arrayt){
+            String newdir = newTxtDir.getAbsolutePath().concat("\\").concat(txtFile.getName());
+            System.out.println(newdir);
+          //  txtFile.renameTo(new File(newdir));
         }
 
-        
+        File[] arrayOfDir = path.listFiles(); //get list of dir
+        for (File file : arrayOfDir) //for each file check if it is a dir
+         { if (file.isDirectory()){ //if it is a dir - get files .txt
+            
+                File[] arrayOftxt =  file.listFiles(new TxtFilter());
+                for (File txtFile : arrayOftxt){ //for each file.txt replace it to new dir
+                    String newdir = newTxtDir.getAbsolutePath().concat("\\").concat(txtFile.getName());
+                    txtFile.renameTo(new File(newdir));
+                   // System.out.println(newdir);
+                }
+                file.delete();
+            }
+         }
     }
 }
 
@@ -44,3 +43,4 @@ class TxtFilter implements FilenameFilter{
         return name.endsWith(".txt");
     }
 }
+
